81179e0b90682616e302f21fbcbe2b4f24e3637e
ZLN <zawlinnaing52@gmail.com>
2021-06-10 22:51:40 +0630

    Js Homework

---
+++ if.js
@@ -0,0 +1,10 @@
+yoursMoeny = 1000000;
+if(yoursMoeny > 900000){
+    console.log("Please usually Donate Foods and Money to Other People during these days")
+}else if (yoursMoeny < 800000){
+    console.log("Please often Donate a few amount of money to Other People during these days")
+}else if (yoursMoeny < 500000){
+    console.log("You can donate if you want or not ")
+}else{
+    console.log("You should save yours money during this current condition and do not waste your money.You can also donate if you want");
+}
---
+++ switch.js
@@ -0,0 +1,18 @@
+let situation = "morning"
+switch (situation) {
+    case "morining":
+        console.log("Eat Breakfast and Prepare to go work.");
+        break;
+    case "Afternoon":
+        console.log("Eat Lunch and take a rest for a few minutes and then Continue to do work");
+        break;
+    case "Evening":
+        console.log("It is time to go back home");
+        break;
+    case "Night":
+        console.log("Eat Dinner, take a rest and then go to sleep");
+        break;
+    default:
+        console.log("I am sleeping now Zzz");
+        break;
+}
